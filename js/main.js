0cc5b239593280ee6382d7b917dae698848fe229
VladimirFetisov <vladimir@one-v.co.il>
2022-09-02 11:11:49 +0300

    while and metki

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-console.log('start');
+"usestrict";
 
-test = val => alert(val)
+// for (let i = 0; i < 3; i++) {
+//     console.log(i + ' i');
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j + ' j');
+        
+//     }
+// }
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 5; k++) {
+            if (k === 2) continue first;
+            console.log(`Third level: ${k}`);
+        }
+    }
+}
