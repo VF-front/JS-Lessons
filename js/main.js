93b9d6c544e165e8f1efb409b6dcb4b893d5b6c9
VladimirFetisov <vladimir@one-v.co.il>
2022-09-07 16:43:53 +0300

    like this task - Elka

--- js/main.js
+++ js/main.js
@@ -1,56 +1,19 @@
 "usestrict";
 
-// Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд.
-// Должен получиться точно такой же массив
+const lines = 18;
+let result = '';
+// Проверяется именно переменная result, формируйте строку в ней
 
-function firstTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
-
-    // Пишем решение вот тут
-    arr.map(item => result.push(item))
-    
-    // Не трогаем
-    return result;
-}
-
-// Измените данный массив так, чтобы все числа были увеличены в 2 раза, а если попадается строка строка - то к ней было добавлено " - done".
-// Для определения типа данных используйте typeof();
-// Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
-
-function secondTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-    // Пишем решение вот тут
-
-    for (let i = 0; i < data.length; i++) {
-        if( typeof data[i] == 'number' ) {
-            data[i] = data[i] * 2;
-        } else {
-            data[i] = data[i] + ' - done'
-        }
+let s = '*';
+let bsp;
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += ' '
     }
-
-    // Не трогаем
-    return data;
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += '*'
+    }
+    result += '\n'
 }
 
-// Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
-// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
-
-function thirdTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
-
-    // Пишем решение вот тут
-
-    data.map(item => result.unshift(item))
-
-    // Не трогаем
-    return result;
-}
-
-// console.log(thirdTask());
+console.log(result);
