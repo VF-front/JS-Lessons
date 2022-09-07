0ba6e2c2d174d62471aced83d4d711644528dc4a
VladimirFetisov <vladimir@one-v.co.il>
2022-09-07 14:50:21 +0300

    work with arr

--- js/main.js
+++ js/main.js
@@ -1,39 +1,56 @@
 "usestrict";
 
-function firstTask(num) {
+// Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд.
+// Должен получиться точно такой же массив
 
-    for (let i = 5; i <= num; i++) {
-        console.log(i)
-    }
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
 
+    // Пишем решение вот тут
+    arr.map(item => result.push(item))
+    
+    // Не трогаем
+    return result;
 }
 
-secondTask = num => {
-    for (let i = 20; i >= num; i--) {
-        if (i === 13) break;
-        console.log(i)
-    }
-}
+// Измените данный массив так, чтобы все числа были увеличены в 2 раза, а если попадается строка строка - то к ней было добавлено " - done".
+// Для определения типа данных используйте typeof();
+// Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
 
-thirdTask = num => {
-    for (let i = 1; i <= num; i++) {
-        if( i % 2) continue
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
 
-        console.log(i);
-    }
-}
+    // Пишем решение вот тут
 
-fourthTask = num => {
-    let i = 2;
-    while ( i <= num ) {
-        if ( i % 2 ) {
-            console.log(i)
+    for (let i = 0; i < data.length; i++) {
+        if( typeof data[i] == 'number' ) {
+            data[i] = data[i] * 2;
+        } else {
+            data[i] = data[i] + ' - done'
         }
-        i++;
     }
+
+    // Не трогаем
+    return data;
 }
 
-fourthTask(16);
-// thirdTask(10);
-// secondTask(10);
-// firstTask(10);
+// Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
+// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    // Пишем решение вот тут
+
+    data.map(item => result.unshift(item))
+
+    // Не трогаем
+    return result;
+}
+
+// console.log(thirdTask());
