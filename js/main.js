70629c78e6153525a1145e62aff52b4d061138ff
VladimirFetisov <vladimir@one-v.co.il>
2022-09-07 12:46:19 +0300

    test 2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,39 @@
 "usestrict";
 
-// for (let i = 0; i < 3; i++) {
-//     console.log(i + ' i');
-//     for (let j = 0; j < 3; j++) {
-//         console.log(j + ' j');
-        
-//     }
-// }
+function firstTask(num) {
 
-first: for (let i = 0; i < 3; i++) {
-    console.log(`First level: ${i}`);
-    for (let j = 0; j < 3; j++) {
-        console.log(`Second level: ${j}`);
-        for (let k = 0; k < 5; k++) {
-            if (k === 2) continue first;
-            console.log(`Third level: ${k}`);
-        }
+    for (let i = 5; i <= num; i++) {
+        console.log(i)
+    }
+
+}
+
+secondTask = num => {
+    for (let i = 20; i >= num; i--) {
+        if (i === 13) break;
+        console.log(i)
     }
 }
+
+thirdTask = num => {
+    for (let i = 1; i <= num; i++) {
+        if( i % 2) continue
+
+        console.log(i);
+    }
+}
+
+fourthTask = num => {
+    let i = 2;
+    while ( i <= num ) {
+        if ( i % 2 ) {
+            console.log(i)
+        }
+        i++;
+    }
+}
+
+fourthTask(16);
+// thirdTask(10);
+// secondTask(10);
+// firstTask(10);
